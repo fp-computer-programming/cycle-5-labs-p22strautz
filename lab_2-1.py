5b242c676f0c4247ccba20030ca28e64e3d29ee7
p22strautz <p22strautz@fairfieldprep.org>
2021-10-25 09:53:07 -0400

    lab 2-1

---
+++ lab_2-1.py
@@ -0,0 +1,14 @@
+# Author: SCT (ADMG) 10/25/21
+
+import math
+
+# 1
+print(math.ceil(7.15))
+# 2
+print(math.floor(19.156))
+# 3
+print(math.fabs(-42))
+# 4
+print(math.pow(2, 3))
+# 5
+print(math.log10(100))
