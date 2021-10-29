a003ef858261a58f07ced915021febd1e549a8b1
p22strautz <p22strautz@fairfieldprep.org>
2021-10-29 10:45:37 -0400

    lab 6-1, 6-2

---
+++ lab_6-1.py
@@ -0,0 +1,9 @@
+# Author: SCT (ADMG) 10/29/21
+
+word = "flibbertigibbet"
+
+print(word.find("t"))
+
+word2 = "I wish, I wish, I was a fish."
+
+print(word2.split(","))
---
+++ lab_6-2.py
@@ -0,0 +1,7 @@
+# Author: SCT (ADMG) 10/29/21
+
+name = str(input("What is your first name?\n"))
+
+name2 = str(input("What is your first name?\n"))
+
+print("Hello, {0}. My name is {1}".format(name, name2))
